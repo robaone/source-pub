ab1eb9610cea2c0141c43aaa73f115a77224a71f
Ansel Robateau <ansel@casechek.com>
2022-05-13 09:45:00 -0500

    feat: add script to clean up local branches

---
+++ UsefulScripts/git_clean_branches.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#################################################
+# Clean up your local git branches by removing
+# all branches except the long lived ones.
+#
+# For example, if you have just finished working
+# on a feature branch and you have published it
+# you can run this and it will delete it for you
+# without you having to type in the branch name.
+#
+# Tip: Checkout your main branch first (i.e. main)
+#################################################
+
+echo "You are about to delete the following branches"
+git branch | grep -v alpha | grep -v beta | grep -v develop | grep -v main
+if [ "$?" == "0" ]; then
+  read -p "Are you sure? " -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ ! $REPLY =~ ^[Yy]$ ]]
+  then
+     exit 1
+  fi
+  git branch | grep -v alpha | grep -v beta | grep -v develop | grep -v main | awk '{print "git branch -D " $1}' | sh
+else
+  exit 2
+fi
