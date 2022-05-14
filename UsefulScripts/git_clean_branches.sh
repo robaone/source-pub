012b8a5b6ec7c54b1aae9352fc3bda55cc63e4b4
Ansel Robateau <robaone@gmail.com>
2022-05-14 09:28:07 -0500

    feat: 🎸 add better messaging (#33)

--- UsefulScripts/git_clean_branches.sh
+++ UsefulScripts/git_clean_branches.sh
@@ -12,16 +12,19 @@
 # Tip: Checkout your main branch first (i.e. main)
 #################################################
 
-echo "You are about to delete the following branches"
-git branch | grep -v alpha | grep -v beta | grep -v develop | grep -v main
-if [ "$?" == "0" ]; then
+BRANCH_LIST=$(git branch | grep -v alpha | grep -v beta | grep -v develop | grep -v main)
+if [ "$?" == "0" ] && [ "$(echo $BRANCH_LIST | wc -l | xargs )" != "0" ]; then
+  echo "You are about to delete the following branches"
+  echo $BRANCH_LIST
+
   read -p "Are you sure? " -n 1 -r
   echo    # (optional) move to a new line
   if [[ ! $REPLY =~ ^[Yy]$ ]]
   then
      exit 1
   fi
-  git branch | grep -v alpha | grep -v beta | grep -v develop | grep -v main | awk '{print "git branch -D " $1}' | sh
+  echo $BRANCH_LIST | awk '{print "git branch -D " $1}' | sh
 else
+  echo "Nothing to do"
   exit 2
 fi
