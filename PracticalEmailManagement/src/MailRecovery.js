106da4343f0467f1cc8ee8487dc4492d54ab0e0b
Ansel Robateau <robaone@gmail.com>
2022-05-25 08:38:16 -0500

    test: get test to pass

--- PracticalEmailManagement/src/MailRecovery.js
+++ PracticalEmailManagement/src/MailRecovery.js
@@ -1,13 +1,25 @@
 // start-app-script
 class MailRecovery {
-  constructor(){
-
+  constructor(emailService,queryService){
+    this.emailService = emailService;
+    this.queryService = queryService;
   }
   run(){
+    const queries = this.queryService.getQueries();
+    const emails = queries.map(
+      query => this.emailService.search(query).map(email => {
+      return this.emailService.move(email,'Inbox');
+    })).flat().filter(x => x !== undefined);
+    console.log(JSON.stringify(emails));
     return {
-      recovered:[{title:'',from:''}],
-      queries:['']
+      recovered:emails,
+      queries
     }
+  }
+  move(query){
+    return this.emailService.search(query).map(email => {
+      return this.emailService.move(email,'Inbox');
+    });
   }
 }
 // end-app-script
--- PracticalEmailManagement/test/MailRecovery.test.js
+++ PracticalEmailManagement/test/MailRecovery.test.js
@@ -1,31 +1,47 @@
 const MailRecovery = require('../src/MailRecovery');
 
+
 test('can be declared', () => {
   const service = new MailRecovery();
 });
 
-test('recover emails matching query', () => {
-  const mailRecovery = new MailRecovery();
-  // Given emails in other folder with data:
-  const emails = [
-    {
-      title:'This is spam',
-      from:'spam@email.com'
-    },
-    {
-      title:'This is good',
-      from:'myfriend@email.com'
+describe('recovering emails', () => {
+  beforeEach(() => {
+    this.emailService = {
+      search: jest.fn(),
+      move: jest.fn()
+    };
+    this.queryService = {
+      getQueries: jest.fn()
     }
-  ];
-  // And queries with data:
-  const queries = [
-    'from:myfriend@email.com'
-  ];
-  // When the timer is triggered
-  const result = mailRecovery.run();
-  // Then the following emails are moved to the inbox
-  expect(result).toEqual({
-    recovered:[{title:'This is good',from:'myfriend@email.com'}],
-    queries:['from:myfriend@email.com']
+    this.service = new MailRecovery(this.emailService,this.queryService);
   })
-})
+  test('recover emails matching query', () => {
+    const mailRecovery = this.service;
+    // Given emails in other folder with data:
+    const emails = [
+      {
+        title:'This is spam',
+        from:'spam@email.com'
+      },
+      {
+        title:'This is good',
+        from:'myfriend@email.com'
+      }
+    ];
+    this.emailService.search.mockReturnValueOnce(emails);
+    this.emailService.move.mockReturnValueOnce(emails[1]);
+    // And queries with data:
+    const queries = [
+      'from:myfriend@email.com'
+    ];
+    this.queryService.getQueries.mockReturnValue(queries);
+    // When the timer is triggered
+    const result = mailRecovery.run();
+    // Then the following emails are moved to the inbox
+    expect(result).toEqual({
+      recovered:[{title:'This is good',from:'myfriend@email.com'}],
+      queries:['from:myfriend@email.com']
+    })
+  })
+});
