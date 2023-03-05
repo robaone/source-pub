773a96c80dc405602a724c05bb25592a5b1b83e0
Ansel Robateau <robaone@gmail.com>
2023-03-05 09:03:43 -0600

    test: flesh out testing scenarios

--- scripts/tests/start_release_test.sh
+++ scripts/tests/start_release_test.sh
@@ -45,7 +45,7 @@ echo Scenario: Start a release by creating the release branch and the pull reque
 
 export PREDICTED_VERSION=1.0.0
 export ARGUMENT_FILE=$(mktemp)
-export MOCK_RESPONSES='[{"stdout":"","exit":0},{"stdout":""},{"stdout":""},{"stdout":""},{"stdout":"CREATED"},{"stdout":""},{"stdout":"CREATED"}]'
+export MOCK_RESPONSES='[{"stdout":"","exit":0},{"stdout":""},{"stdout":""},{"stdout":"CREATED"},{"stdout":""},{"stdout":"CREATED"}]'
 export MOCK_TRACKING_FILE=$(mktemp)
 export GIT_PATH="$SCRIPT_DIR/mock_cmd.sh"
 export GH_PATH=$SCRIPT_DIR/mock_cmd.sh
@@ -62,3 +62,72 @@ pr list --base main --head release/v1.0.0 --json number --jq .[0].number
 pr create --base main --head release/v1.0.0 --title Release v1.0.0 to main --body Release v1.0.0
 pr list --base develop --head release/v1.0.0 --json number --jq .[0].number
 pr create --base develop --head release/v1.0.0 --title Release v1.0.0 to develop --body Release v1.0.0" "$(cat $ARGUMENT_FILE)"
+
+echo Scenario: Start a release by creating the release branch and the pull requests where the release branch already exists and the pull request to main already exists
+
+# GIVEN
+
+export PREDICTED_VERSION=1.0.0
+export ARGUMENT_FILE=$(mktemp)
+export MOCK_RESPONSES='[{"stdout":"","exit":0},{"stdout":""},{"stdout":"47"},{"stdout":""},{"stdout":"CREATED"}]'
+export MOCK_TRACKING_FILE=$(mktemp)
+export GIT_PATH="$SCRIPT_DIR/mock_cmd.sh"
+export GH_PATH=$SCRIPT_DIR/mock_cmd.sh
+
+# WHEN
+
+ACTUAL_RESULT=$($CMD)
+
+# THEN
+
+assert_equals "checkout release/v1.0.0
+push -u origin release/v1.0.0
+pr list --base main --head release/v1.0.0 --json number --jq .[0].number
+pr list --base develop --head release/v1.0.0 --json number --jq .[0].number
+pr create --base develop --head release/v1.0.0 --title Release v1.0.0 to develop --body Release v1.0.0" "$(cat $ARGUMENT_FILE)"
+
+echo Scenario: Start a release by creating the release branch and the pull requests where the release branch already exists and the pull request to develop already exists
+
+# GIVEN
+
+export PREDICTED_VERSION=1.0.0
+export ARGUMENT_FILE=$(mktemp)
+export MOCK_RESPONSES='[{"stdout":"","exit":0},{"stdout":""},{"stdout":""},{"stdout":"CREATED"},{"stdout":"47"}]'
+export MOCK_TRACKING_FILE=$(mktemp)
+export GIT_PATH="$SCRIPT_DIR/mock_cmd.sh"
+export GH_PATH=$SCRIPT_DIR/mock_cmd.sh
+
+# WHEN
+
+ACTUAL_RESULT=$($CMD)
+
+# THEN
+
+assert_equals "checkout release/v1.0.0
+push -u origin release/v1.0.0
+pr list --base main --head release/v1.0.0 --json number --jq .[0].number
+pr create --base main --head release/v1.0.0 --title Release v1.0.0 to main --body Release v1.0.0
+pr list --base develop --head release/v1.0.0 --json number --jq .[0].number" "$(cat $ARGUMENT_FILE)"
+
+
+echo Scenario: Start a release by creating the release branch and the pull requests where the release branch already exists and the pull requests already exist
+
+# GIVEN
+
+export PREDICTED_VERSION=1.0.0
+export ARGUMENT_FILE=$(mktemp)
+export MOCK_RESPONSES='[{"stdout":"","exit":0},{"stdout":""},{"stdout":"47"},{"stdout":"47"}]'
+export MOCK_TRACKING_FILE=$(mktemp)
+export GIT_PATH="$SCRIPT_DIR/mock_cmd.sh"
+export GH_PATH=$SCRIPT_DIR/mock_cmd.sh
+
+# WHEN
+
+ACTUAL_RESULT=$($CMD)
+
+# THEN
+
+assert_equals "checkout release/v1.0.0
+push -u origin release/v1.0.0
+pr list --base main --head release/v1.0.0 --json number --jq .[0].number
+pr list --base develop --head release/v1.0.0 --json number --jq .[0].number" "$(cat $ARGUMENT_FILE)"
