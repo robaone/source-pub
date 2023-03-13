cd33dc08c1c79118653962085ebd0f37f68321ba
Ansel Robateau <robaone@gmail.com>
2023-03-13 08:37:40 -0500

    Feature/79 support inter project dependency triggering (#80)
    * docs: ✏️ update readme file
    
    ✅ Closes: 79
    
    * test: add unit test
    
    * feat: add script to determine projects
    
    * feat: 🎸 add dependency to list of projects
    
    ✅ Closes: 79

---
+++ tooling/scripts/tests/build_depends_project_list_test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+CMD=$1
+
+if [ "$CMD" == "" ]; then
+    echo "Usage: $0 [command]"
+    exit 1
+fi
+
+function assert_equals {
+  if [ "$1" != "$2" ]; then
+    echo "Expected: $1"
+    echo "Actual:   $2"
+    exit 1
+  else
+    echo "OK"
+  fi
+}
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+echo Scenario: Build the list of projects that depend on the given files
+
+# GIVEN
+
+PROJECT2_DEPENDS_CONTENTS="projects/project1/*"
+
+export MOCK_ARGUMENT_FILE=$(mktemp)
+export MOCK_RESPONSES='[{"stdout":"projects/project2/.depends"},{"stdout":"'$PROJECT2_DEPENDS_CONTENTS'"}]'
+export MOCK_TRACKING_FILE=$(mktemp)
+export FIND_PATH="$SCRIPT_DIR/mock_cmd.sh"
+export CAT_PATH="$SCRIPT_DIR/mock_cmd.sh"
+
+FILES="projects/project1/file1.txt
+README.md"
+EXPECTED_RESULTS="project2"
+
+# WHEN
+
+ACTUAL_RESULT="$(echo "$FILES" | $CMD)"
+
+# THEN
+
+assert_equals "$EXPECTED_RESULTS" "$ACTUAL_RESULT"
