8e38f39ff60c772e7461e359f8c10e4cb7b9b0fa
Ansel Robateau <robaone@gmail.com>
2023-04-27 15:01:25 -0500

    remove branch name assignment

--- projects/practical-email-management/scripts/run-unit-tests.sh
+++ projects/practical-email-management/scripts/run-unit-tests.sh
@@ -5,7 +5,6 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 export OWNER=roboane
 export REPO=source-pub
 export WORKFLOW_NAME="Practical Email Management"
-export BRANCH_NAME=$(git branch --show-current)
 
 bash -xv $SCRIPT_DIR/../../../tooling/scripts/gh_trigger_workflow_dispatch.sh
 exit $?
