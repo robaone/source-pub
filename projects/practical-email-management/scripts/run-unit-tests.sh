de2fc37c11c21ff5d9a4b58abe92b772d47ebbf1
Ansel Robateau <robaone@gmail.com>
2023-04-27 14:54:03 -0500

    use the full name of the workflow

--- projects/practical-email-management/scripts/run-unit-tests.sh
+++ projects/practical-email-management/scripts/run-unit-tests.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 export OWNER=roboane
 export REPO=source-pub
-export WORKFLOW_NAME=practical_email_management
+export WORKFLOW_NAME="Practical Email Management"
 
 bash -xv $SCRIPT_DIR/../../../tooling/scripts/gh_trigger_workflow_dispatch.sh
 exit $?
