689e76bb5b843cb76872082431f01f6b7013d413
Ansel Robateau <robaone@gmail.com>
2023-05-03 14:03:26 -0500

    ci: fix errors

--- tooling/scripts/determine_skip_e2e.sh
+++ tooling/scripts/determine_skip_e2e.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PR_INFO="$(gh pr view $EVENT_NUMBER --json bodY)"
+PR_INFO="$(gh pr view $EVENT_NUMBER --json body)"
 echo "## PR Info" >&2
 echo "$PR_INFO" | jq -r '.body' >&2
 SKIP_E2E=$(echo "$PR_INFO" | jq -r '.body' | grep '\[x\] Skip e2e')
