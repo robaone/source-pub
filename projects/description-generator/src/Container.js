be714ebcecabaefc6d98229bc7c482e45310fbf8
Ansel Robateau <robaone@gmail.com>
2024-02-04 07:51:41 -0600

    feat: add to container

--- projects/description-generator/src/Container.js
+++ projects/description-generator/src/Container.js
@@ -1,8 +1,11 @@
 class Container {
   constructor() {
     this.SLACK_WEBHOOK_URL = 'slack_webhook_url';
+    this.STORY_TEMPLATE = 'story_template';
+    this.BUG_TEMPLATE = 'bug_template';
     this.spreadsheet = this.getSpreadsheet();
     this.slackApi = this.getSlackApi(UrlFetchApp.fetch);
+    this.generator = this.getDescriptionGenerator();
   }
   getSpreadsheet() {
     const sheet = SpreadsheetApp.getActiveSpreadsheet();
@@ -11,6 +14,19 @@ class Container {
   getSlackApi(fetch) {
     const api = new SlackApi(fetch,PropertiesService.getScriptProperties().getProperty(this.SLACK_WEBHOOK_URL));
     return api;
+  }
+  getDescriptionGenerator() {
+    const container = this;
+    const open_ai = {
+      call: (prompt) => {
+        container.slackApi.send(`**OpenAI Prompt:\n\n${prompt}`);
+        return "Message sent";
+      }
+    }
+    const story_template = JSON.parse(PropertiesService.getScriptProperties().getProperty(this.STORY_TEMPLATE));
+    const bug_template = JSON.parse(PropertiesService.getScriptProperties().getProperty(this.BUG_TEMPLATE));
+    const generator = new DescriptionGenerator(open_ai, story_template, bug_template);
+    return generator;
   }
 }
 
