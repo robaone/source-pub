5012f959b745870474c9c388f6ebf655342a453b
Ansel Robateau <robaone@gmail.com>
2024-02-04 07:38:45 -0600

    feat: add slackapi class

--- projects/description-generator/src/Container.js
+++ projects/description-generator/src/Container.js
@@ -1,9 +1,19 @@
 class Container {
   constructor() {
+    this.SLACK_WEBHOOK_URL = 'slack_webhook_url';
     this.spreadsheet = this.getSpreadsheet();
+    this.slackApi = this.getSlackApi(UrlFetchApp.fetch);
   }
   getSpreadsheet() {
     const sheet = SpreadsheetApp.getActiveSpreadsheet();
     return sheet;
   }
+  getSlackApi(fetch) {
+    const api = new SlackApi(fetch,PropertiesService.getScriptProperties().getProperty(this.SLACK_WEBHOOK_URL));
+    return api;
+  }
+}
+
+function test_container() {
+  const container = new Container();
 }
