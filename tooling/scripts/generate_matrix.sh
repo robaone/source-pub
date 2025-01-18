154e87b2caf94fd3ce391374f50f6e53c6af435a
Ansel Robateau <robaone@gmail.com>
2025-01-18 15:09:17 -0600

    feat: 🎸 revert the script by removing the new changes (#248)
    ✅ Closes: PD-46
    
    Co-authored-by: Ansel Robateau <ansel.robateau@redshelf.com>

--- tooling/scripts/generate_matrix.sh
+++ tooling/scripts/generate_matrix.sh
@@ -47,10 +47,6 @@ function is_empty_string() {
   fi
 }
 
-function git_repo_root_path() {
-  git rev-parse --show-toplevel
-}
-
 if [ "$(is_empty_string "$PROJECTS")" == "1" ]; then
   echo "{\"include\":[]}"
   exit 0
@@ -81,8 +77,7 @@ function get_workflow_os () {
 function get_targets () {
   local project_json="$1"
   local job=$2
-  local root_path=$3
-  local project_path="${root_path}/$project"
+  local project_path="$SCRIPT_DIR/../../projects/$project"
   local targets=$(echo "$project_json" | jq -rc .workflow.${job}.targets 2>/dev/null)
   if [ "$targets" == "" ]; then
     targets="null"
@@ -123,16 +118,15 @@ function get_workflow() {
 
 function generate_matrix_object() {
   local projects="$1"
-  local root_path="$2"
   local matrix_object="{\"include\":["
   for project in $projects; do
-    local project_path="${root_path}/$project"
+    local project_path="$SCRIPT_DIR/../../projects/$project"
     if [ "$(file_exists "$project_path/package.json")" != "1" ]; then
       continue
     fi
     local project_json=$($CAT_CMD "$project_path/package.json")
     local WORKFLOW_OS=$(get_workflow_os "$project_json" "$JOB_NAME")
-    TARGETS=$(get_targets "$project_json" "$JOB_NAME" "$root_path")
+    TARGETS=$(get_targets "$project_json" "$JOB_NAME")
     if [ "$TARGETS" != "[]" ]; then
       local index=0
       while [ $index -lt $(echo "$TARGETS" | jq -r '. | length') ]; do
@@ -173,10 +167,4 @@ if [ "$JOB_NAME" == "featureTest" ] && [ "$SKIP_E2E" == "true" ]; then
   exit 0
 fi
 
-if [ "$PROJECT_ROOT" == "" ] || [ "$PROJECT_ROOT" == "." ]; then
-  PROJECT_PATH=$(git_repo_root_path)
-else
-  PROJECT_PATH=$(git_repo_root_path)/$PROJECT_ROOT
-fi
-
-generate_matrix_object "$PROJECTS" "$PROJECT_PATH"
+generate_matrix_object "$PROJECTS"
